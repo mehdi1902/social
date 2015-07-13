97b6b9413312e116cbff43ef682473502e025c4c
pegahgl <golchin_p@yahoo.com>
2015-07-13 17:53:32 +0430

    yuhahaha

--- ask_to_go/views.py
+++ ask_to_go/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 
 # Create your views here.
+def mainpage (request):
+    name = 'duset daram'
+    return render_to_response ('index.html',locals())
+
--- social/settings.py
+++ social/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'social.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -101,7 +101,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static','static_root')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static','static_dirs'),
+)
+
 
 TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,  'templates'),
+    os.path.join(BASE_DIR,'static'),
 )
